60b5b4c17d4553ca8d18ea308daad0b9852e6ce1
Tim Abell <tim@timwise.co.uk>
2016-04-08 09:19:50 +0100

    blank console proj

---
+++ ef-complex-pk/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ef_complex_pk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
