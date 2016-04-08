60eee742ee02532429e290f00a080b61d0b8c512
Tim Abell <tim@timwise.co.uk>
2016-04-08 09:51:26 +0100

    basic db context with one table

---
+++ ef-complex-pk/TestDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+
+namespace ef_complex_pk
+{
+    public class TestDbContext: DbContext
+    {
+        public IDbSet<Widget> Widgets { get; set; }
+    }
+
+    public class Widget
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ ef-complex-pk/TestRunner.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace ef_complex_pk
+{
+    [TestFixture]
+    public class TestRunner
+    {
+        [Test]
+        public void RunIt()
+        {
+            using (var context = new TestDbContext())
+            {
+                context.Widgets.Count();
+            }
+        }
+    }
+}
