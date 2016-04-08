0b727d31bae1135a3320b077104a65c47effac54
Tim Abell <tim@timwise.co.uk>
2016-04-08 09:58:22 +0100

    simple broken complex type Id
    System.Data.Entity.ModelConfiguration.ModelValidationException : One or more validation errors were detected during model generation:
    
    ef_complex_pk.Widget: : EntityType 'Widget' has no key defined. Define the key for this EntityType.
    Widgets: EntityType: EntitySet 'Widgets' is based on type 'Widget' that has no keys defined.

--- ef-complex-pk/TestDbContext.cs
+++ ef-complex-pk/TestDbContext.cs
@@ -5,11 +5,22 @@ namespace ef_complex_pk
     public class TestDbContext: DbContext
     {
         public IDbSet<Widget> Widgets { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.ComplexType<IdWrap>();
+            //modelBuilder.Entity<Widget>().HasKey(e => e.WidgetId);
+        }
     }
 
     public class Widget
     {
-        public int Id { get; set; }
+        public IdWrap WidgetId { get; set; }
         public string Name { get; set; }
+    }
+
+    public class IdWrap
+    {
+        public int IdWrapId { get; set; }
     }
 }
